14dedd0ddece09c7e086ab2727c7d86ca839f361
mody-1090 <nicegoout@gmail.com>
2025-02-22 23:22:13 +0300

    Delete app directory

--- app/auth/api_routes.py
+++
@@ -1,9 +0,0 @@
-# app/routes/api_routes.py
-from flask import Blueprint, jsonify
-
-api_routes = Blueprint('api_routes', __name__)
-
-@api_routes.route('/data')
-def get_data():
-    data = {"message": "Hello from API"}
-    return jsonify(data)
--- app/services/notification_service.py
+++
@@ -1,8 +0,0 @@
-from app import db
-from app.models import Notification
-
-def create_notification(user_id, message):
-    """إنشاء إشعار جديد للمستخدم"""
-    notification = Notification(user_id=user_id, message=message)
-    db.session.add(notification)
-    db.session.commit()
